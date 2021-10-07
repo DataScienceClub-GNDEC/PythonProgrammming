97d88932244bcd678a31c3a2ac30537a9146a64e
Vrishti Gupta <ivrishtigupta@gmail.com>
2021-10-07 23:30:21 +0530

    Pattern

---
+++ Print_Your_Font.py
@@ -0,0 +1,116 @@
+name = input("Enter your name: ")
+
+for i in range(len(name)):
+    character = name[i]
+    character = character.upper()
+     
+    if (character == "A"):
+        print("..######..\n..#....#..\n..######..", end = " ")
+        print("\n..#....#..\n..#....#..\n\n")
+         
+    elif (character == "B"):
+        print("..######..\n..#....#..\n..#####...", end = " ")
+        print("\n..#....#..\n..######..\n\n")
+         
+    elif (character == "C"):
+        print("..######..\n..#.......\n..#.......", end = " ")
+        print("\n..#.......\n..######..\n\n")
+         
+    elif (character == "D"):
+        print("..#####...\n..#....#..\n..#....#..", end = " ")
+        print("\n..#....#..\n..#####...\n\n")
+         
+    elif (character == "E"):
+        print("..######..\n..#.......\n..#####...", end = " ")
+        print("\n..#.......\n..######..\n\n")
+         
+    elif (character == "F"):
+        print("..######..\n..#.......\n..#####...", end = " ")
+        print("\n..#.......\n..#.......\n\n")
+         
+    elif (character == "G"):
+        print("..######..\n..#.......\n..#.####..", end = " ")
+        print("\n..#....#..\n..#####...\n\n")
+         
+    elif (character == "H"):
+        print("..#....#..\n..#....#..\n..######..", end = " ")
+        print("\n..#....#..\n..#....#..\n\n")
+         
+    elif (character == "I"):
+        print("..######..\n....##....\n....##....", end = " ")
+        print("\n....##....\n..######..\n\n")
+         
+    elif (character == "J"):
+        print("..######..\n....##....\n....##....", end = " ")
+        print("\n..#.##....\n..####....\n\n")
+         
+    elif (character == "K"):
+        print("..#...#...\n..#..#....\n..##......", end = " ")
+        print("\n..#..#....\n..#...#...\n\n")
+         
+    elif (character == "L"):
+        print("..#.......\n..#.......\n..#.......", end = " ")
+        print("\n..#.......\n..######..\n\n")
+         
+    elif (character == "M"):
+        print("..#....#..\n..##..##..\n..#.##.#..", end = " ")
+        print("\n..#....#..\n..#....#..\n\n")
+         
+    elif (character == "N"):
+        print("..#....#..\n..##...#..\n..#.#..#..", end = " ")
+        print("\n..#..#.#..\n..#...##..\n\n")
+         
+    elif (character == "O"):
+        print("..######..\n..#....#..\n..#....#..", end = " ")
+        print("\n..#....#..\n..######..\n\n")
+         
+    elif (character == "P"):
+        print("..######..\n..#....#..\n..######..", end = " ")
+        print("\n..#.......\n..#.......\n\n")
+         
+    elif (character == "Q"):
+        print("..######..\n..#....#..\n..#.#..#..", end = " ")
+        print("\n..#..#.#..\n..######..\n\n")
+         
+    elif (character == "R"):
+        print("..######..\n..#....#..\n..#.##...", end = " ")
+        print("\n..#...#...\n..#....#..\n\n")
+         
+    elif (character == "S"):
+        print("..######..\n..#.......\n..######..", end = " ")
+        print("\n.......#..\n..######..\n\n")
+         
+    elif (character == "T"):
+        print("..######..\n....##....\n....##....", end = " ")
+        print("\n....##....\n....##....\n\n")
+         
+    elif (character == "U"):
+        print("..#....#..\n..#....#..\n..#....#..", end = " ")
+        print("\n..#....#..\n..######..\n\n")
+         
+    elif (character == "V"):
+        print("..#....#..\n..#....#..\n..#....#..", end = " ")
+        print("\n...#..#...\n....##....\n\n")
+         
+    elif (character == "W"):
+        print("..#....#..\n..#....#..\n..#.##.#..", end = " ")
+        print("\n..##..##..\n..#....#..\n\n")
+         
+    elif (character == "X"):
+        print("..#....#..\n...#..#...\n....##....", end = " ")
+        print("\n...#..#...\n..#....#..\n\n")
+         
+    elif (character == "Y"):
+        print("..#....#..\n...#..#...\n....##....", end = " ")
+        print("\n....##....\n....##....\n\n")
+         
+    elif (character == "Z"):
+        print("..######..\n......#...\n.....#....", end = " ")
+        print("\n....#.....\n..######..\n\n")
+         
+    elif (character == " "):
+        print("..........\n..........\n..........", end = " ")
+        print("\n..........\n\n")
+         
+    elif (character == "."):
+        print("----..----\n\n")
